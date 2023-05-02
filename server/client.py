3c254ca12f1f1d071e08b4a2ab563f56e9408120
pythango-anuj <2018034@iiitdmj.ac.in>
2023-05-02 16:12:06 +0530

    Basic Implementation

---
+++ server/client.py
@@ -0,0 +1,21 @@
+import socketio
+import asyncio
+
+
+sio_client=socketio.AsyncClient()
+
+@sio_client.event
+async def connect():
+    print('I am connected!')
+
+@sio_client.event
+async def disconnect():
+    print('I am disconnected!')
+
+
+async def main():
+    await sio_client.connect(url='http://localhost:8000/', socketio_path='sockets')
+    await sio_client.disconnect()
+
+if __name__=='__main__':
+    asyncio.run(main())
---
+++ server/main.py
@@ -0,0 +1,27 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from sockets import sio_app
+
+
+app = FastAPI()
+app.mount('/', app=sio_app)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+@app.get('/')
+async def home():
+    return {'message' : "Hello my developers"}
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='localhost', port=8000 ,reload=True)
